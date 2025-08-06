492762668e05c3a7720184791ff622cb61c5db5a
Alex Camacho <acamach2@cisco.com>
2025-08-06 09:17:56 -0600

    feat: configure mongo atlas connection

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restx import Api
 
-
 def create_app():
     """
     Create and configure Flask application with Swagger documentation.
